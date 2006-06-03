e925d7fe158e52a05243478d701ed96a95f64565
mamo-net <mamo-net@8385ffbd-b80d-0410-a6ce-fb0b7b73164d>
2006-06-03 22:46:49 +0000

    Removed old installer.

--- viscacha/install/db/postratings.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `{:=DBPREFIX=:}postratings` (
-  `mid` mediumint(7) NOT NULL,
-  `aid` mediumint(7) NOT NULL,
-  `tid` int(10) NOT NULL,
-  `pid` int(10) NOT NULL,
-  `rating` enum('-1','1') NOT NULL,
-  UNIQUE KEY `mid` (`mid`,`pid`)
-) TYPE=MyISAM;
