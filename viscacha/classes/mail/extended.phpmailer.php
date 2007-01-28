007a89d96a7a982d4013657bd54183f5a7aa4b72
mamo-net <mamo-net@8385ffbd-b80d-0410-a6ce-fb0b7b73164d>
2007-01-28 14:48:48 +0000

    Removed "viscacha" (moved to "viscacha08")

--- viscacha/classes/mail/extended.phpmailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-class my_phpmailer extends phpmailer {
-
-    // Replace the default error_handler
-    function error_handler($msg) {
-		trigger_error($msg, E_USER_WARNING);
-    }
-}
-?>
--- viscacha/install/package/update/steps.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$steps = array(
-	1 => 'Initialize Upgrade Process',
-	2 => 'Prepare Filesystem',
-	3 => 'Update Filesystem and Database',
-	4 => 'Complete Upgrade'
-);
-?>
