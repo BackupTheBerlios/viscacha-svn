1487578e78bbad5f77cd70057ab8a4c37d227873
mamo-net <mamo-net@8385ffbd-b80d-0410-a6ce-fb0b7b73164d>
2012-01-26 22:44:15 +0000

    jQuery Rating plugin fixes

--- Framework/client/scripts/jquery/unminified/jquery.rating.js
+++ Framework/client/scripts/jquery/unminified/jquery.rating.js
@@ -1,14 +1,16 @@
-//
-// rating Plugin
-// By Chris Richards
-// Last Update: 6/21/2011
-// Turns a select box into a star rating control.
-//
+/**
+ * jQuery Rating Plugin
+ *
+ * Turns a select box into a star rating control.
+ *
+ * Author:       Chris Richards
+ * Contributors: Dovy Paukstys
+ *               Adrian Macneil
+ */
 
-(function ($) { 
-	
-	$.fn.rating = function(options)
-	{
+(function ($) {
+
+	$.fn.rating = function(options) {
 		var settings =
 		{
 			showCancel: true,
@@ -17,120 +19,105 @@
 			startValue: null,
 			disabled: false
 		};
+		if (options) { $.extend( settings, options); }
+
 		var methods = {
-		   hoverOver: function(evt)
-			{
+		   hoverOver: function(evt) {
 				var elm = $(evt.target);
-				if( elm.hasClass("ui-rating-cancel") ) {
-					elm.addClass("ui-rating-cancel-full");
-				} 
-				else {
+				if (elm.hasClass("ui-rating-cancel")) {
+					elm.attr("class", "ui-rating-cancel ui-rating-cancel-full");
+				} else {
 					elm.prevAll().andSelf()
 						.not(".ui-rating-cancel")
 						.addClass("ui-rating-hover");
 				}
 			},
-			hoverOut: function(evt)
-			{
+			hoverOut: function(evt) {
 				var elm = $(evt.target);
-				//Are we over the Cancel or the star?
-				if( elm.hasClass("ui-rating-cancel") ) {
-					elm.addClass("ui-rating-cancel-empty")
-						.removeClass("ui-rating-cancel-full");
-				}
-				else {
+				if (elm.hasClass("ui-rating-cancel")) {
+					elm.attr("class", "ui-rating-cancel ui-rating-cancel-empty");
+				} else {
 					elm.prevAll().andSelf()
 						.not(".ui-rating-cancel")
 						.removeClass("ui-rating-hover");
 				}
 			},
-			click: function(evt)
-			{
+			click: function(evt) {
 				var elm = $(evt.target);
 				var value = settings.cancelValue;
-				//Are we over the Cancel or the star?
-				elm.parents(".content-box-content:first").removeClass('formerror');
-				if( elm.hasClass("ui-rating-cancel") ) {
-					methods.empty(elm, elm.parent());
-				}
-				else {
-					elm.closest(".ui-rating-star").prevAll().andSelf()
-						.not(".ui-rating-cancel")
-						.prop("className", "ui-rating-star ui-rating-full");
-					elm.closest(".ui-rating-star").nextAll()
-						.not(".ui-rating-cancel")
-						.prop("className", "ui-rating-star ui-rating-empty");
+				if (elm.hasClass("ui-rating-cancel")) {
+					elm.siblings(".ui-rating-star")
+						.attr("class", "ui-rating-star ui-rating-empty");
+					elm.attr("class", "ui-rating-cancel ui-rating-cancel-empty");
+				} else {
+					elm.prevAll().andSelf().not(".ui-rating-cancel")
+						.attr("class", "ui-rating-star ui-rating-full");
+					elm.nextAll().not(".ui-rating-cancel")
+						.attr("class", "ui-rating-star ui-rating-empty");
 					elm.siblings(".ui-rating-cancel")
-						.prop("className", "ui-rating-cancel ui-rating-cancel-empty");
-					value = elm.attr("value");
+						.attr("class", "ui-rating-cancel ui-rating-cancel-empty");
+					value = elm.attr("data-value");
 				}
-				if( !evt.data.hasChanged ) {
-					$(evt.data.selectBox).val( value ).trigger("change");
+
+				if (!evt.data.hasChanged) {
+					$(evt.data.selectBox).val(value).trigger("change");
 				}
 			},
-			change: function(evt)
-			{
-				var value = $(this).val();
-				methods.setValue(value, evt.data.container, evt.data.selectBox);
-//				if (isDirty)
-//					isDirty = true;
+			change: function(evt) {
+				methods.setValue($(this).val(), evt.data.container, evt.data.selectBox);
 			},
-			setValue: function(value, container, selectBox)
-			{
+			setValue: function(value, container, selectBox) {
 				var evt = {"target": null, "data": {}};
-				evt.target = $(".ui-rating-star[id="+ selectBox.attr('id') + "_" + value + "]", container);
+
+				evt.target = $(".ui-rating-star[data-value="+ value +"]", container);
 				evt.data.selectBox = selectBox;
 				evt.data.hasChanged = true;
 				methods.click(evt);
-			},
-			empty: function(elm, parent)
-			{
-				parent.find('.ui-rating-star').removeClass('ui-rating-full');
-				parent.find('.ui-rating-star').addClass('ui-rating-empty');				
-				elm.prop("className", "ui-rating-cancel ui-rating-cancel-empty")
-					.nextAll().prop("className", "ui-rating-star ui-rating-empty");
 			}
- 
 		};
 
 		return this.each(function() {
-			var self = $(this), elm, val;
+			var self = $(this);
+
 			if ('select-one' !== this.type) { return; }
-			if (self.prop('hasProcessed')) { return; }
-			if (options) { $.extend( settings, options); }
+			if (self.data('rating-loaded')) { return; }
+
 			self.hide();
-			self.prop('hasProcessed', true);
-			elm = $("<div/>").prop({
-				className: "ui-rating"
-			}).insertAfter( self );
+			self.data('rating-loaded', true);
+
+			var elm = $(document.createElement("div")).attr({
+				"class": "ui-rating"
+			}).insertAfter(self);
 			$('option', self).each(function() {
-				if(this.value!="") {
-					$("<a/>").prop({
-						className: "ui-rating-star ui-rating-empty",
-						title: $(this).text(),
-						id: self.attr('id') + '_' + this.value
+				if (this.value !== "") {
+					$(document.createElement("a")).attr({
+						"class": "ui-rating-star ui-rating-empty",
+						"title": $(this).text(),
+						"data-value": this.value
 					}).appendTo(elm);
 				}
 			});
-			if (true == settings.showCancel) {
-				$("<a/>").prop({
-					className: "ui-rating-cancel ui-rating-cancel-empty",
-					title: settings.cancelTitle
+			if (settings.showCancel) {
+				$(document.createElement("a")).attr({
+					"class": "ui-rating-cancel ui-rating-cancel-empty",
+					"title": settings.cancelTitle
 				}).appendTo(elm);
 			}
-			if ( 0 !==  $('option:selected', self).size() ) {
-				methods.setValue( self.val(), elm, self );
+			if (self.val() !== "") {
+				methods.setValue(self.val(), elm, self);
 			} else {
-				val = null !== settings.startValue ? settings.startValue : settings.cancelValue;
-				methods.setValue( val, elm, self );
-				self.val(val);
+				var value = settings.startValue !== null ? settings.startValue : settings.cancelValue;
+				methods.setValue(value, elm, self);
+				self.val(value);
 			}
-			if( true !== settings.disabled && self.prop("disabled") !== true ) {
+
+			if (settings.disabled === false && self.is(":disabled") === false) {
 				$(elm).bind("mouseover", methods.hoverOver)
 					.bind("mouseout", methods.hoverOut)
-					.bind("click",{"selectBox": self}, methods.click);
-			}	
-			self.bind("change", {"selectBox": self, "container": elm},  methods.change);
+					.bind("click", {"selectBox": self}, methods.click);
+			}
+
+			self.bind("change", {"selectBox": self, "container": elm}, methods.change);
 		});
 	};
 })(jQuery);
